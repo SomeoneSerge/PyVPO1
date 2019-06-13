1bfef216bfb963389bca3665f92105bf26bec0a3
Serge Kozlukov <newkozlukov@gmail.com>
2019-06-13 16:14:02 +0300

    SheetMachine tests and names

--- vpo1/tests/test_sheetmachine.py
+++ vpo1/tests/test_sheetmachine.py
@@ -15,8 +15,10 @@ def test_is_colnumbers_row():
     assert not is_colnumbers_row([1, 2, 3.14])
     assert not is_colnumbers_row([1, 2, 'name', 'price', 'whatever'])
 
-def test_SheetMachine_valid_sheet():
-    example_sheet = [
+
+@pytest.fixture
+def voronezh_parsed():
+    sheet = [
             ['Воронежская область', None, None],
             ['Государственные, Муниципальные', None, None],
             ['очная', None, None],
@@ -25,10 +27,26 @@ def test_SheetMachine_valid_sheet():
             [1, 2, 5],
             ['Математика', 1, 27]
             ]
-    example_sheet = list(map(VPO1Row, example_sheet))
-    events = list(SheetMachine().read_sheet(example_sheet))
-    assert ('region', 'Воронежская область') in events
-    assert ('funded_by', 'Государственные, Муниципальные') in events
-    assert ('time_involvement', 'очная') in events
-    assert next((True for event, payload in events if event=='section' and payload[0]=='2.1.2'))
-    assert next((True for event, payload in events if event=='row' and payload['rowname'] == 'Математика'))
+    sheet = list(map(VPO1Row, sheet))
+    events =  list(SheetMachine().read_sheet(sheet))
+    return events
+
+def test_SheetMachine_region(voronezh_parsed):
+    assert ('region', 'Воронежская область') in voronezh_parsed
+def test_SheetMachine_funded_by(voronezh_parsed):
+    assert ('funded_by', 'Государственные, Муниципальные') in voronezh_parsed
+def test_SheetMachine_time_involvement(voronezh_parsed):
+    assert ('time_involvement', 'очная') in voronezh_parsed
+def test_SheetMachine_section(voronezh_parsed):
+    assert next((
+        True for event, section in voronezh_parsed
+        if event=='section' and section.number=='2.1.2'))
+def test_SheetMachine_df_row_exists(voronezh_parsed):
+    assert next((
+        True for event, payload in voronezh_parsed
+        if event=='row' and payload.rowname == 'Математика'))
+def test_SheetMachine_df_row_colindices(voronezh_parsed):
+    mathrow = next((
+        payload for event, payload in voronezh_parsed
+        if event=='row' and payload.rowname == 'Математика'))
+    assert mathrow.data == [(5, 27)]
